9437bd0606a6a93cc544c22b4ea752868f99e392
Emir0zcelik <themirozcelik@gmail.com>
2024-02-28 12:56:58 +0300

    Renderer class

--- Solo-Core/src/Renderer/Renderer.cpp
+++ Solo-Core/src/Renderer/Renderer.cpp
@@ -4,3 +4,12 @@ void Renderer::Clear() const
 {
     glClear(GL_COLOR_BUFFER_BIT);
 }
+
+void Renderer::Draw(const VAO& va, const IBO& ib, const Shader& shader) const
+{
+    shader.Bind();
+    va.Bind();
+    ib.Bind();
+
+    glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
+}
--- Solo-Core/src/Renderer/Renderer.h
+++ Solo-Core/src/Renderer/Renderer.h
@@ -2,6 +2,7 @@
 #include "IBO.h"
 #include "VAO.h"
 #include "VBOLayout.h"
+#include "Shader.h"
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
@@ -11,5 +12,7 @@ class Renderer
     public:
 
         void Clear() const;
+
+        void Draw(const VAO& va, const IBO& ib, const Shader& shader) const;
 
 };
